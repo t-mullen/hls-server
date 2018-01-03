11ddb27a6a15c277e1846facf49189835ea23be6
RationalCoding <thomasmullendesign@gmail.com>
2018-01-03 13:47:15 -0500

    refactor

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ var path = require('path')
 var zlib = require('zlib')
 var httpAttach = require('http-attach')
 
+var CONTENT_TYPE = {
+  MANIFEST: 'application/vnd.apple.mpegurl',
+  SEGMENT: 'video/MP2T',
+  HTML: 'text/html'
+}
+
 function HLSServer (server, opts) {
   var self = this
   if (!(self instanceof HLSServer)) return new HLSServer(server, opts)
@@ -20,15 +26,15 @@ HLSServer.prototype.attach = function (server, opts) {
   opts = opts || {}
   self.path = opts.path || self.path || '/'
   self.dir = opts.dir || self.dir || ''
-  self.debugPlayer = opts.debugPlayer || true
+  self.debugPlayer = opts.debugPlayer == null ? true : opts.debugPlayer
 
-  if (!isNaN(server)) { // Port numbers
+  if (isNaN(server)) {
+    httpAttach(server, self._middleware.bind(self))
+  } else {  // Port numbers
     var port = server
     server = http.createServer()
     httpAttach(server, self._middleware.bind(self))
     server.listen(port)
-  } else {
-    httpAttach(server, self._middleware.bind(self))
   }
 }
 
@@ -38,14 +44,11 @@ HLSServer.prototype._middleware = function (req, res, next) {
   var uri = url.parse(req.url).pathname
   var relativePath = path.relative(self.path, uri)
   var filePath = path.join(self.dir, relativePath)
-  var ext = path.extname(filePath)
+  var extension = path.extname(filePath)
 
   if (uri === '/player.html' && self.debugPlayer) {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.write('<html><head><title>Debug Player</title></head><body>')
-    res.write('<video src="http://localhost:' + 8000 + '/out2.m3u8" controls autoplay></body></html>')
-    res.end()
-    return next()
+    self._writeDebugPlayer(res, next)
+    return
   }
 
   fs.exists(filePath, function (exists) {
@@ -53,33 +56,12 @@ HLSServer.prototype._middleware = function (req, res, next) {
       res.writeHead(404)
       res.end()
     } else {
-      switch (ext) {
+      switch (extension) {
         case '.m3u8':
-          fs.readFile(filePath, function (err, contents) {
-            if (err || !contents) { // Error or empty playlist
-              res.writeHead(500)
-              res.end()
-              return next()
-            }
-
-            res.writeHead(200, {'Content-Type': 'application/vnd.apple.mpegurl'})
-            var ae = req.headers['accept-encoding']
-
-            if (ae.match(/\bgzip\b/)) { // Gzip support
-              zlib.gzip(contents, function (err, zip) {
-                if (err) throw err
-
-                res.writeHead(200, {'content-encoding': 'gzip'})
-                res.end(zip)
-              })
-            } else {
-              res.end(contents, 'utf-8')
-            }
-          })
+          self._writeManifest(filePath, req, res, next)
           break
         case '.ts':
-          res.writeHead(200, { 'Content-Type': 'video/MP2T' })
-          fs.createReadStream(filePath, { bufferSize: 64 * 1024 }).pipe(res)
+          self._writeSegment(filePath, res, next)
           break
         default:
           next()
@@ -87,4 +69,60 @@ HLSServer.prototype._middleware = function (req, res, next) {
       }
     }
   })
+}
+
+HLSServer.prototype._writeDebugPlayer = function (res, next) {
+  res.writeHead(200, { 'Content-Type': CONTENT_TYPE.HTML })
+  // TODO: Use HLS.js
+  res.write(`
+    <html>
+    <head><title>Debug Player</title></head>
+      <body>
+        <video src="" controls autoplay></video>
+        <br>
+        <input type="text" />
+        <script>
+          document.querySelector("input").addEventListener("keyup", function () {
+            document.querySelector("video").src = document.querySelector("input").value
+          })
+        </script>
+      </body>
+    </html>`)
+  res.end()
+  next()
+}
+
+HLSServer.prototype._writeManifest = function (filePath, req, res, next) {
+  fs.readFile(filePath, function (err, contents) {
+    if (err || !contents) { // Error or empty playlist
+      res.writeHead(500)
+      res.end()
+      return next()
+    }
+
+    res.writeHead(200, {'Content-Type': CONTENT_TYPE.MANIFEST})
+    var ae = req.headers['accept-encoding']
+
+    if (ae.match(/\bgzip\b/)) { // Gzip support
+      zlib.gzip(contents, function (err, zip) {
+        if (err) {
+          res.writeHead(500)
+          res.end()
+          return next()
+        }
+
+        res.writeHead(200, {'content-encoding': 'gzip'})
+        res.end(zip)
+        next()
+      })
+    } else {
+      res.end(contents, 'utf-8')
+      next()
+    }
+  })
+}
+
+HLSServer.prototype._writeSegment = function (filePath, res, next) {
+  res.writeHead(200, { 'Content-Type': CONTENT_TYPE.SEGMENT })
+  fs.createReadStream(filePath, { bufferSize: 64 * 1024 }).pipe(res)
 }
