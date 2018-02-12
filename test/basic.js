4b5203bd0c92e1178091865bd1665062567ef94c
RationalCoding <thomasmullendesign@gmail.com>
2018-02-11 19:58:38 -0500

    add some request tests, test for CORS (all good)

--- test/basic.js
+++ test/basic.js
@@ -2,6 +2,8 @@ var test = require('tape')
 var HLSServer = require('./../src/index')
 var http = require('http')
 var ffmpeg = require('fluent-ffmpeg')
+var httpAttach = require('http-attach')
+var request = require('request')
 
 var PORT = 8000
 var hls
@@ -43,8 +45,55 @@ test('constructor with http server', function (t) {
 
   var httpServer = http.createServer()
   var hls2 = new HLSServer(httpServer)
-  httpServer.listen()
-  t.ok(hls2)
+  httpServer.listen(9081)
+  t.ok(hls2, 'Server not null')
+
+  request({
+    url: 'http://127.0.0.1:9082/test/files/output/out.m3u8'
+  }, function (error, response, body) {
+    if (error) t.fail('Manifest failed with error ' + error)
+    if (response.statusCode !== 200) t.fail('Wrong status code ' + response.statusCode)
+    console.log(response.statusCode)
+    console.log(body)
+    t.pass('Manifest fetched.')
+  })
+})
+
+test('constructor with http server (CORS)', function (t) {
+  t.plan(2)
+
+  var httpServer = http.createServer()
+  function addCors (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Request-Method', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET')
+    res.setHeader('Access-Control-Allow-Headers', '*')
+    if (req.method === 'OPTIONS') {
+      res.writeHead(200)
+      res.end()
+      return
+    }
+    next()
+  }
+  httpAttach(httpServer, addCors)
+  var hls2 = new HLSServer(httpServer, {
+
+  })
+  httpServer.listen(9082)
+  t.ok(hls2, 'Server not null.')
+
+  request({
+    url: 'http://127.0.0.1:9082/test/files/output/out.m3u8',
+    headers: {
+      Origin: 'http://NOTTHERIGHTORIGIN.example'
+    }
+  }, function (error, response, body) {
+    if (error) t.fail('Manifest failed with error ' + error)
+    if (response.statusCode !== 200) t.fail('Wrong status code ' + response.statusCode)
+    console.log(response.statusCode)
+    console.log(body)
+    t.pass('Manifest fetched.')
+  })
 })
 
 test('SUMMARY', function (t) {
