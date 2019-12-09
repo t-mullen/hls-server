d51b5f41786343b2f0581fcbb37e0fa100e33970
Thomas Mullen <14932492+t-mullen@users.noreply.github.com>
2019-12-09 00:39:27 -0500

    Update basic.js

--- test/basic.js
+++ test/basic.js
@@ -1,35 +1,11 @@
 var test = require('tape')
 var HLSServer = require('./../src/index')
 var http = require('http')
-var ffmpeg = require('fluent-ffmpeg')
 var httpAttach = require('http-attach')
 var request = require('request')
 
 var PORT = 8000
 var hls
-
-var ffmpegHLS = function (inputPath, outputPath, cb) {
-  return ffmpeg(inputPath, { timeout: 432000 }).addOptions([
-    '-profile:v baseline',
-    '-level 3.0',
-    '-s 640x360',
-    '-start_number 0',
-    '-hls_time 10',
-    '-hls_list_size 0',
-    '-f hls'
-  ]).output(outputPath).on('end', cb).run()
-}
-
-test('ffmpeg', function (t) {
-  t.plan(2)
-
-  ffmpegHLS('test/files/out.mp4', 'test/files/output/out.m3u8', function () {
-    t.pass('mp4 done')
-  })
-  ffmpegHLS('test/files/out2.webm', 'test/files/output/out2.m3u8', function () {
-    t.pass('webm done')
-  })
-})
 
 test('constructor without http server', function (t) {
   t.plan(1)
